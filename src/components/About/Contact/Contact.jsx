ced70e334d7334559dc3b11c2198cd54ffb4788f
moukim hfaidh <moukimhfaid@gmail.com>
2021-12-07 05:25:55 +0100

    adding founder section

--- src/components/About/Contact/Contact.jsx
+++ src/components/About/Contact/Contact.jsx
@@ -6,20 +6,11 @@ function Contact({contactRef}) {
         <div className="container">
           <div className="nk-block nk-block-about">
             <div className="
-          row
-          justify-content-between
-          align-items-center
-          gutter-vr-50px
+         row justify-content-center text-center
         ">
-              <div className="col-lg-6">
               <div className="nk-block-text">
                   <div className="nk-block-text-head">
-                    <h2 data-delay="0.7" className="title title-lg ttu">
-                     The Founder:
-                    </h2>
-                    <p data-delay="0.8">
-                    Our founder is Ali Essa, a young college student that is extremely passionate about Ethereum Classic, He is of Egyptian descent and currently resides in the United States he has always been interested in CryptoCurrency and NFTS and most importantly $ETC, he is currently working as an operations manager for multiple businesses and he is planning to pursue a degree in Software engineering due to his extreme interest in software 
-                    </p>
+                  
                     {/* <h3 className="text-white">Contract Address</h3>
                     <a target="_blank" rel="noreferrer" href="https://blockscout.com/etc/mainnet/address/0x59E34EF31049565D041Aec6137F40f518c2D47c1/contracts"><strong>
                     0x59E34EF31049565D041Aec6137F40f518c2D47c1
@@ -29,7 +20,7 @@ function Contact({contactRef}) {
                 </div>
                 <div className="nk-block-text">
                   <div className="nk-block-text-head">
-                    <h2 data-delay="0.7" className="title title-lg ttu">
+                    <h2 data-delay="0.7" className="title title-xl roadmap__gelato">
                       Contact Us
                     </h2>
                     <p data-delay="0.8">
@@ -44,59 +35,9 @@ function Contact({contactRef}) {
                   </div>
                 </div>
               </div>
-              <div className="col-lg-5 text-center order-lg-first">
-                <div data-delay="0.1" className="nk-block-contact nk-block-contact-s1">
-                  <ul className="contact-list">
-                    <li data-delay="0.3">
-                      <em className="contact-icon fas fa-envelope" />
-                      <div className="contact-text">
-                        <span>info@Classic Pharaohs .com</span>
-                      </div>
-                    </li>
-                    <a target="_blank" rel="noreferrer" href="https://www.instagram.com/alii.essa/"><li data-delay="0.4">
-                        <em className="contact-icon fab fa-instagram" />
-                        <div className="contact-text">
-                          <span>Join us on Instagram</span>
-                        </div>
-                      </li></a>
-                    <a target="_blank" rel="noreferrer" href="https://twitter.com/ali22essa"><li data-delay="0.4">
-                        <em className="contact-icon fab fa-twitter" />
-                        <div className="contact-text">
-                          <span>Join us on Twitter</span>
-                        </div>
-                      </li></a>
-                    <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/ali-m-essa-9b7165223"><li data-delay="0.4">
-                        <em className="contact-icon fab fa-linkedin" />
-                        <div className="contact-text">
-                          <span>Join us on Linkedin</span>
-                        </div>
-                      </li></a>
-  
-                  </ul>
-                  <div className="
-                nk-circle-animation nk-df-center
-                text-primary
-                small
-              " />
-                </div>
-                <ul data-delay="0.6" className="
-              social-links social-links-s2
-              justify-content-center
-            ">
-                  <li>
-                    <a target="_blank" rel="noreferrer" href="https://www.instagram.com/alii.essa/"><em className="fab fa-instagram" /></a>
-                  </li>
-                  <li>
-                    <a target="_blank" rel="noreferrer" href="https://twitter.com/ali22essa "><em className="fab fa-twitter" /></a>
-                  </li>
-                  <li>
-                    <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/ali-m-essa-9b7165223"><em className="fab fa-linkedin" /></a>
-                  </li>
-                </ul>
-              </div>
+         
             </div>
           </div>
-        </div>
       </section>
     )
 }
--- src/components/About/Founder/Founder.jsx
+++ src/components/About/Founder/Founder.jsx
@@ -5,15 +5,15 @@ function Founder() {
     <section id="apes" className="section">
       <div className="container">
         <div className="row justify-content-center text-center">
-          <div className="col-lg-6">
+          <div className="col-lg-12">
             <div className="section-head section-head-s2">
               <h2 title="Bored Ape Collection" className="title title-xl">
                 The Founder
               </h2>
-              <p className="animated">
+              <p className="animated shadow-sm" style={{fontSize:"22px"}}>
                 Our founder is Ali Essa, a young college student that is
                 extremely passionate about Ethereum Classic, He is of Egyptian
-                descent and currently resides in the United States he has always
+                descent and currently reside in the united states he has always
                 been interested in CryptoCurrency and NFTS and most importantly
                 $ETC, he is currently working as an operations manager for
                 multiple businesses and he is planning to pursue a degree in
@@ -27,7 +27,7 @@ function Founder() {
           src="/images/founder.jpg"
           width="300"
           height="300"
-          style={{width: "300px", height: "auto"}}
+          style={{width: "300px", height: "auto" , border: "5px solid white"}}
           className="figure-img mx-auto img-fluid rounded"
           alt="The founder."
         />
